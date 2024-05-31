e67161b27ac55760821696da88a638a864ee9145
lhyeonnit <lhyeonnit@gmail.com>
2024-05-31 17:28:02 +0900

    user

--- src/main/java/shop/mtcoding/loginapp/user/UserController.java
+++ src/main/java/shop/mtcoding/loginapp/user/UserController.java
@@ -1,4 +1,39 @@
 package shop.mtcoding.loginapp.user;
 
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RequiredArgsConstructor
+@Controller
 public class UserController {
+
+    private final UserService userService;
+    private final HttpSession session; // IoC 등록되어 있음 (스프링 실행 되면)
+
+
+    @GetMapping("/join-form")
+    public String joinForm(){
+        return "user/join-form";
+    }
+
+    @GetMapping("/login-form")
+    public String loginForm(){
+        return "user/login-form";
+    }
+
+    @PostMapping("/join")
+    public String join(String username, String password, String email){
+        userService.doJoin(username, password, email);
+        return "redirect:/login-form";
+    }
+
+    @PostMapping("/login")
+    public String login(String username, String password){
+        User sessionUser = userService.doLogin(username, password);
+        session.setAttribute("sessionUser", sessionUser); // 로그인 되면 세션 저장
+        return "redirect:/shop";
+    }
 }
--- src/main/java/shop/mtcoding/loginapp/user/UserRepository.java
+++ src/main/java/shop/mtcoding/loginapp/user/UserRepository.java
@@ -1,4 +1,8 @@
 package shop.mtcoding.loginapp.user;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUsername(@Param("username") String username);
 }
