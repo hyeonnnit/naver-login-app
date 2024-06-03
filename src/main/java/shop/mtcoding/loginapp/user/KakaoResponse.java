e8857833c467117ec511cc1d2402906c12725409
lhyeonnit <lhyeonnit@gmail.com>
2024-06-03 09:43:50 +0900

    KakaoResponse

---
+++ src/main/java/shop/mtcoding/loginapp/user/KakaoResponse.java
@@ -0,0 +1,39 @@
+package shop.mtcoding.loginapp.user;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.security.Timestamp;
+import java.util.Properties;
+
+public class KakaoResponse {
+    @Data // getter, setter
+    public static class TokenDTO {
+        @JsonProperty("access_token")
+        private String accessToken;
+        @JsonProperty("token_type")
+        private String tokenType;
+        @JsonProperty("refresh_token")
+        private String refreshToken;
+        @JsonProperty("expires_in")
+        private Integer expiresIn;
+        private String scope;
+        @JsonProperty("refresh_token_expires_in")
+        private Integer refreshTokenExpiresIn;
+    }
+
+    @Data
+    public static class KakaoUserDTO {
+        private Long id;
+        @JsonProperty("connected_at")
+        private Timestamp connectedAt;
+        private Properties properties;
+
+        @Data
+        class Properties {
+            private String nickname;
+        }
+
+    }
+
+}
--- src/main/java/shop/mtcoding/loginapp/user/UserController.java
+++ src/main/java/shop/mtcoding/loginapp/user/UserController.java
@@ -16,6 +16,7 @@ public class UserController {
     // http://localhost:8080/oauth/callback?code=3u9fk
     @GetMapping("/oauth/callback")
     public String oauthCallback(String code) {
+        System.out.println("callback::"+code);
         User sessionUser = userService.kakaoLogin(code);
         session.setAttribute("sessionUser", sessionUser);
         return "redirect:/shop";
